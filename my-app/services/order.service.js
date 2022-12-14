1099904925529d41f5bb17c4319a6311d4bd06b2
Eden Ezra <edene@10bis.co.il>
2022-12-14 17:03:49 +0200

    fix

--- my-app/services/order.service.js
+++ my-app/services/order.service.js
@@ -1,8 +1,6 @@
 const Order = require("../models/order.model");
 const Product = require("../models/product.model");
 const shoppingCartService = require("../services/shoppingCart.service")
-const AddressModel = require('../models/address.model');
-const { findOneAndUpdate } = require("../models/address.model");
 const ObjectId = require('mongoose').Types.ObjectId;
 
 
@@ -20,7 +18,6 @@ async function createOrder(orderObject) {
 
     for (let product of cart.products) {
         order.products.push({ product: product.product, count: product.count })
-        const productDbObject = await Product.findOne({ _id: ObjectId(product.product._id) });
     }
 
     await shoppingCartService.deleteShoppingCart(cart._id);
